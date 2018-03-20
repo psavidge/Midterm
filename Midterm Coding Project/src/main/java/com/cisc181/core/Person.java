a2104d978945b8a5f9d9f25f3f40ff411bacc6dd
psavi_000 <psavi_000@128.4.151.155>
2018-03-20 16:58:35 -0400

    commit to repository

--- Midterm Coding Project/src/main/java/com/cisc181/core/Person.java
+++ Midterm Coding Project/src/main/java/com/cisc181/core/Person.java
@@ -46,8 +46,14 @@ public abstract class Person implements java.io.Serializable {
 		return DOB;
 	}
 
-	public void setDOB(Date DOB){
-		this.DOB = DOB;
+	public void setDOB(Date DOB) throws PersonException{
+		if (DOB.getYear() < 1918) {
+			throw new PersonException("Invalid Date Of Birth", this);
+		}
+		else
+			{
+			this.DOB = DOB;
+			}
 		
 		
 	}
@@ -60,9 +66,15 @@ public abstract class Person implements java.io.Serializable {
 		return address;
 	}
 
-	public void setPhone(String newPhone_number) {
+	public void setPhone(String newPhone_number)throws PersonException {
+		String regex = "^\\(?([0-9]{3})\\)?[-.\\s]?([0-9]{3})[-.\\s]?([0-9]{4})$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(newPhone_number);
+		if (matcher.matches()) {
 		phone_number = newPhone_number;
-	
+		}
+		else throw new PersonException("Invalid Phone Number",this);
+		
 	}
 
 	public String getPhone() {
@@ -89,7 +101,7 @@ public abstract class Person implements java.io.Serializable {
 	 */
 
 	public Person(String FirstName, String MiddleName, String LastName,
-			Date DOB, String Address, String Phone_number, String Email)
+			Date DOB, String Address, String Phone_number, String Email) throws PersonException
 	{
 		this.FirstName = FirstName;
 		this.MiddleName = MiddleName;
--- Midterm Coding Project/src/test/java/com/cisc181/core/Staff_Test.java
+++ Midterm Coding Project/src/test/java/com/cisc181/core/Staff_Test.java
@@ -4,19 +4,67 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.cisc181.eNums.eTitle;
+
 public class Staff_Test {
 
+	private static ArrayList<Staff> staffList = new ArrayList<Staff>();
+	
 	@BeforeClass
-	public static void setup() {
+	public static void salarySetup() 
+	{		
+		double firstSalary = 100;
+		for(int i = 0; i < 5; i++)
+		{
+			Staff s = new Staff(null);
+			s.setSalary(firstSalary);
+			staffList.add(s);
+			firstSalary += 100;
+		}
 	}
 	
 	@Test
-	public void test() {
-		assertEquals(1,1);
-	}	
-
+	public void test() 
+	{
+		int total = 0;
+		int averageSalary = 0;
+		for(int j = 0; j < staffList.size(); j++)
+		{
+			total += staffList.get(j).getSalary();
+		}
+		
+		averageSalary = total / staffList.size();
+		
+		assertEquals(averageSalary, 300);
+	}
+	@Test
+	public void test2()
+	{
+		int res = 0;
+		try {
+			Staff AgeTest = new Staff("Patrick","James","Savidge",new Date(1896,12,27),"55 Delaware St","123-456-7890","psav@udel.edu","7-9",500,10000000,new Date(2016,3,20),eTitle.MR);
+		} catch (PersonException e) {
+		res = 1;
+		}
+		assertEquals(res,1);
+	}
+	@Test
+	public void test3()
+	{
+		int isFalse = 0;
+		try {
+			Staff PhoneTest = new Staff("Patrick","James","Savidge",new Date(1996,12,27),"55 Delaware St","123-456-789","psav@udel.edu","7-9",500,10000000,new Date(2016,3,20),eTitle.MR);
+		} catch (PersonException e) {
+			isFalse = 1;
+		}
+		assertEquals(isFalse,1);
+	}
 }
+
+	
+	
